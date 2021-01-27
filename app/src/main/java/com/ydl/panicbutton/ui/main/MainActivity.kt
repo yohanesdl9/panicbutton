9486c22d0269439d42b9f0e9885666602b93034a
Yohanes Dwi Listio <yohanes@digiponic.co.id>
2021-01-27 15:21:25 +0700

    Adding FusedLocationProviderClient

--- app/src/main/java/com/ydl/panicbutton/ui/main/MainActivity.kt
+++ app/src/main/java/com/ydl/panicbutton/ui/main/MainActivity.kt
@@ -1,13 +1,73 @@
 package com.ydl.panicbutton.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.ydl.panicbutton.R
 import com.ydl.panicbutton.ancestors.BaseActivity
+import com.ydl.panicbutton.extension.instantiateViewModel
+import com.ydl.panicbutton.ui.login.LoginActivity
+import org.jetbrains.anko.startActivity
 
 class MainActivity : BaseActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val vm by lazy { instantiateViewModel<MainVM>() }
+
+    private val handler = Handler()
+
+    val runnable: Runnable = Runnable(){
+        fun run(){
+            try {
+                fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                }
+            } catch (ex: SecurityException) {
+                Log.e("ERROR", ex.message!!)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        handler.postDelayed(runnable, 300000)
+
+        vm.userLogout.observe(this, Observer{
+            vm.preferenceRepository.removeUser()
+            startActivity<LoginActivity>()
+            finishAffinity()
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menuPengaturan -> {
+
+                true
+            }
+            R.id.menuLogout -> {
+                vm.logout()
+                true
+            }
+            else -> super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/ydl/panicbutton/ui/main/MainVM.kt
+++ app/src/main/java/com/ydl/panicbutton/ui/main/MainVM.kt
@@ -1,10 +1,35 @@
 package com.ydl.panicbutton.ui.main
 
+import android.app.AlertDialog
+import com.mcnmr.utilities.wrapper.SingleEventWrapper
 import com.ydl.panicbutton.MainApplication
 import com.ydl.panicbutton.ancestors.BaseActivity
 import com.ydl.panicbutton.ancestors.BaseViewModel
+import com.ydl.panicbutton.repository.network.request.RemoteRepository
+import com.ydl.panicbutton.repository.preference.PreferenceRepository
+import javax.inject.Inject
 
 class MainVM(activity: BaseActivity): BaseViewModel(activity = activity) {
 
+    @Inject
+    lateinit var remoteRepository: RemoteRepository
+
+    @Inject
+    lateinit var preferenceRepository: PreferenceRepository
+
+    val userLogout = SingleEventWrapper<Void>()
+
     init { (activity.application as MainApplication).consumer.inject(this) }
+
+    fun logout(){
+        val alertDialogBuilder = AlertDialog.Builder(activity)
+        alertDialogBuilder.setTitle("Apakah Anda yakin ingin logout?")
+        alertDialogBuilder.setPositiveButton("Ya") { _, _ ->
+            userLogout.trigger()
+        }
+        alertDialogBuilder.setNegativeButton("Tidak") { _, _ ->
+
+        }
+        alertDialogBuilder.show()
+    }
 }
